2f7a19ee2f19704d8fb1d072815b82d7b96be9d4
mkanakis <marioskanakis@gmail.com>
2019-05-05 14:21:34 +0200

    converted audio files, removed prints

--- vsdk/service_development/views/vse_choice.py
+++ vsdk/service_development/views/vse_choice.py
@@ -46,8 +46,6 @@ def choice(request, element_id, session_id):
     session.record_step(choice_element)
     context = choice_generate_context(choice_element, session)
     
-    print(context)
-
     return render(request, 'new_choice.xml', context, content_type='text/xml')
     # return render(request, 'choice.xml', context, content_type='text/xml')
 
@@ -55,9 +53,7 @@ def choice(request, element_id, session_id):
 def post(request, element_id, session_id):
     """
     Saves the chosen choice to the session
-    """
-    print ("HELLOOOOO")
-    
+    """    
     if 'redirect_url' in request.POST:
         redirect_url = request.POST['redirect_url']
     else: raise ValueError('Incorrect request, redirect_url not set')
