c8fd6f0eea32c05a09d7a60b777716b9ff8a67da
mkanakis <marioskanakis@gmail.com>
2019-05-10 14:40:50 +0200

    test

--- vsdk/settings.py
+++ vsdk/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'tk2(l(00&kfe7j97j$dvgz&b6r!kk_zbse1(9w*eoc$bcwu773'
 ########
 # DEBUG = True
 
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = ['*']
@@ -146,7 +146,7 @@ STATICFILES_DIRS = (
 )
 
 MEDIA_ROOT = os.path.join(SITE_ROOT, 'uploads')
-MEDIA_URL = '/uploads/converted/'
+MEDIA_URL = '/uploads/'
 
 
 # Update database configuration with $DATABASE_URL.ALLOWED_HOSTSimport
