5271ccfd8fc4b66410389cde1796120d5dcebfd2
Thodoris Zois <thzois.csd@gmail.com>
2019-05-19 14:23:43 +0200

    - Fix plant decision

--- vsdk/service_development/models/crop.py
+++ vsdk/service_development/models/crop.py
@@ -2,17 +2,17 @@ from django.db import models
 
 class Crop(models.Model):
     name = models.CharField(max_length=60, primary_key = True)
-    clouds = models.FloatField(null = True, blank = True)
-    clouds_hi = models.FloatField(null = True, blank = True)
-    clouds_mid = models.FloatField(null = True, blank = True)
-    clouds_low = models.FloatField(null = True, blank = True)
-    min_temp = models.FloatField(null = True, blank = True)   
-    max_temp = models.FloatField(null = True, blank = True)
-    temp = models.FloatField(null = True, blank = True)
-    precip = models.FloatField(null = True, blank = True)
-    rh = models.FloatField(null = True, blank = True)
-    snow = models.FloatField(null = True, blank = True)
-    snow_depth = models.FloatField(null = True, blank = True)
+    clouds = models.FloatField(default = -1, blank = True)
+    clouds_hi = models.FloatField(default = -1, blank = True)
+    clouds_mid = models.FloatField(default = -1, blank = True)
+    clouds_low = models.FloatField(default = -1, blank = True)
+    min_temp = models.FloatField(default = -1, blank = True)   
+    max_temp = models.FloatField(default = -1, blank = True)
+    temp = models.FloatField(default = -1, blank = True)
+    precip = models.FloatField(default = -1, blank = True)
+    rh = models.FloatField(default = -1, blank = True)
+    snow = models.FloatField(default = -1, blank = True)
+    snow_depth = models.FloatField(default = -1, blank = True)
 
     def save(self, *args, **kwargs):
         self.name = self.name.lower()
